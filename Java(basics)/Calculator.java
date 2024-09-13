7ed2c2ee31a7e7f497b9d81f9bdebdb46009510f
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-09-14 03:41:25 +0530

    update

--- Java(basics)/Calculator.java
+++ Java(basics)/Calculator.java
@@ -1,7 +1,39 @@
 import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
+      
         Scanner in = new Scanner(System.in);
-        
+     
+        int ans=0;
+        while (true) {
+            char ch=in.next().trim().charAt(0);
+            if(ch=='+'|| ch=='-' || ch== '*' || ch=='/'|| ch=='%'){
+                // input two numbers
+                int num1=in.nextInt();
+                int num2=in.nextInt();
+                if(ch=='+'){
+                    ans=num1+num2;
+                }
+                if(ch=='-'){
+                    ans=num1-num2;
+                }
+                if(ch=='*'){
+                    ans=num1*num2;
+                }
+                if(ch=='/'){
+                    
+                    if(num2!=0){
+                        ans=num1/num2;
+                    }
+                }
+                if(ch=='%'){
+                    ans=num1%num2;
+                }
+
+            }
+            System.out.println(ans);
+        }
+      
     }
+  
 }
