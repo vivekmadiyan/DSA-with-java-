0b5cc97c3c5148f0a7cb7db1550d465c6dfd2cbe
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-10-27 00:05:40 +0530

    new

--- Recursion/Fibo.java
+++ Recursion/Fibo.java
@@ -1,7 +1,10 @@
 public class Fibo {
     public static void main(String[] args) {
-    System.out.println(fibo(57));
+    System.out.println(fiboFormula(57));
     }
+    static int fiboFormula(int n){
+     return (int)(Math.pow(((1+ Math.sqrt(5))/2), n)/ Math.sqrt(5));
+        }
     static int fibo(int n){
         if(n<2){
             return n;
