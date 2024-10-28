c48a9d1cf935730256ee3c44d1291dd42029921e
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-10-29 00:05:41 +0530

    Update PowOf2.java

--- Mathh/PowOf2.java
+++ Mathh/PowOf2.java
@@ -1,5 +1,7 @@
 public class PowOf2 {
     public static void main(String[] args) {
-        
+        int n=19;
+        boolean isPow2=  n>0 && ((n-1)&n)==0;
+        System.out.println(isPow2);
     }
 }
