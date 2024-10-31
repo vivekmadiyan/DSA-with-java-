ef38e96b45dda4652cc416d6589e4f99aa25452e
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-10-31 22:50:23 +0530

    Update tempCodeRunnerFile.java

--- Java(basics)/tempCodeRunnerFile.java
+++ Java(basics)/tempCodeRunnerFile.java
@@ -1,1 +1,13 @@
-int[][] arr =new int[3][3];
+class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int n = nums.length;
+//         for (int i = 0; i < n - 1; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return new int[]{i, j};
+//                 }
+//             }
+//         }
+//         return new int[]{};
+//     }
+// }
