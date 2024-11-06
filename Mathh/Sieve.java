18da2e2f201f69defd5b12de0e4d8ba4b2497cc0
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-11-06 22:40:48 +0530

    CH

---
+++ Mathh/Sieve.java
@@ -0,0 +1,13 @@
+public class Sieve {
+    public static void main(String[] args) {
+        int n=40;
+        boolean[] primes = new boolean[n+1];
+        sieve(n, primes);
+    static void sieve(int n, boolean[] primes){
+     for (int i = 2; i *1< =n; i++) {
+    if(!primes[i]){
+     
+    }
+}
+    }
+}
