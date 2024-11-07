01a58e13f75e4be2bec98cce3ce95b2ab4cef3aa
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-11-07 23:49:59 +0530

    Create RemDup.java

---
+++ Arrayquestion/RemDup.java
@@ -0,0 +1,19 @@
+public class RemDup {
+    public static void main(String[] args) {
+        int [] nums={
+            1,1,2
+        };
+      
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        System.out.println(i+1);// to print no of original elements(without duplicate)
+        for(int k=0;k<=i;k++){
+            System.out.print(nums[k]+ " ");
+        }
+    }
+}
