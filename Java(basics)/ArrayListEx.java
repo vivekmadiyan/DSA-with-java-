99c935b2f0226b6cd013d08a625e60a2aab5062f
Vivek Madiyan <147875476+vivekmadiyan@users.noreply.github.com>
2024-11-10 21:47:01 +0530

    Update ArrayListEx.java

--- Java(basics)/ArrayListEx.java
+++ Java(basics)/ArrayListEx.java
@@ -9,7 +9,7 @@ public class ArrayListEx {
         // list.add(10);
         // list.add(11);
         // list.add(12);
-        // list.add(13);
+     
         // list.add(14);
         // list.add(15);
         // list.add(16);
