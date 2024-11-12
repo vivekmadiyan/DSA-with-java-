3ae6a4130db5056e079838ade730ab2d1da90d16
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-11-12 13:38:58 +0530

    Update NewtonSqrt.java

--- Mathh/NewtonSqrt.java
+++ Mathh/NewtonSqrt.java
@@ -1,6 +1,19 @@
 public class NewtonSqrt {
     public static void main(String[] args) {
         //NEWTOON RAPSON METHOD
-        root
+        System.out.println(sqrt(40));
+       
+    }
+    static double sqrt(double n){
+        double x=n;
+        double root;
+        while(true){
+           root=0.5*(x+(n/x));
+            if(Math.abs(root-x)<1){
+                break;
+            }
+            x=root;
+        }
+        return root;
     }
 }
