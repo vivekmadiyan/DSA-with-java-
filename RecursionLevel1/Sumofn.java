ea9eebf04da28151995b8e9997357298d231744e
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-11-17 02:44:01 +0530

    Create Sumofn.java

---
+++ RecursionLevel1/Sumofn.java
@@ -0,0 +1,12 @@
+public class Sumofn {
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+    static int fact(int n){
+     if(n==1||n==0){
+        return n;
+     }
+     return n+fact(n-1);
+    }
+    }
+
