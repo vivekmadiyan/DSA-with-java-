5211d7e92e58cc6757149ad0d078f76396fc289c
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-11-17 02:49:27 +0530

    Update SumOfdigits.java

--- RecursionLevel1/SumOfdigits.java
+++ RecursionLevel1/SumOfdigits.java
@@ -1,6 +1,11 @@
 public class SumOfdigits {
     public static void main(String[] args) {
-        
+        System.out.println(sum(521));
     }
-    
+    static int sum(int n){
+        if(n%10==n){
+            return n;
+        }
+        return n%10+sum(n/10);
+    }
 }
