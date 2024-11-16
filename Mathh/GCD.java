790324bc4b2eb3585d8ef91e00b69d71661772cb
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-11-17 02:43:43 +0530

    change

--- Mathh/GCD.java
+++ Mathh/GCD.java
@@ -1,11 +1,15 @@
 public class GCD {
     public static void main(String[] args) {
-    System.out.println(gcd(4, 8));   
+   System.out.println(gcd(9, 18)); 
+    System.out.println(lcm(9,18));
     }
 static int gcd(int a, int b){
 if(a==0){
     return b;
 }
  return gcd(b%a,a);
+}
+static int lcm(int a,int b){
+    return a*b/gcd(a,b);
 }
 }
