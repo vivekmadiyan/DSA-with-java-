8a1bf9850877a28df2a9cf2830126507f4e2d1fb
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-11-17 02:43:47 +0530

    Create Factorial.java

---
+++ RecursionLevel1/Factorial.java
@@ -0,0 +1,11 @@
+public class Factorial {
+    public static void main(String[] args) {
+    System.out.println(fact(5));
+    }
+    static int fact(int n){
+     if(n==1||n==0){
+        return n;
+     }
+     return n*fact(n-1);
+    }
+}
