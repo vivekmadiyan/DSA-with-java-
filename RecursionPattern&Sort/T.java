6d4af6014cf4942be3890213dd5775280d5a13b2
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-11-26 11:44:39 +0530

    Update T.java

--- RecursionPattern&Sort/T.java
+++ RecursionPattern&Sort/T.java
@@ -1,6 +1,7 @@
 public class T {
     public static void main(String[] args) {
         triangle(4, 0);
+        triangle2(4, 0);
     }
     static void triangle(int r, int c) {
         if (r == 0) {
@@ -12,6 +13,20 @@ public class T {
         } else {
             System.out.println(); // Move to the next line after finishing one row
             triangle(r - 1, 0);
+        }
+    }
+    static void triangle2(int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            // Use print instead of println to stay on the same line
+            triangle2(r, c + 1);
+            System.out.print("*"); 
+        } else {
+            
+            triangle2(r - 1, 0);
+            System.out.println();
         }
     }
 }
