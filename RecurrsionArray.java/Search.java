915757aed8095c283072387d06bf31733a0d4096
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-11-26 05:39:59 +0530

    Update Search.java

--- RecurrsionArray.java/Search.java
+++ RecurrsionArray.java/Search.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
 public class Search {
     public static void main(String[] args) {
-        int[] arr={1,2,3,18,9};
+        int[] arr={1,2,3,18,18,9};
         int target=18;
         int index=0;
-        System.out.println(search(arr, target, index));
-        System.out.println(searchIndex(arr, target, index));
+        // System.out.println(search(arr, target, index));
+        // System.out.println(searchIndex(arr, target, index));
+        ArrayList ans=searchIndexAll(arr, target, index, new ArrayList<>());
+        System.out.println(ans);
         
     }
     //to check if number exists or not
@@ -31,4 +34,32 @@ public class Search {
            return searchIndex(arr, target, index+1);
         }
      }
+    //  static ArrayList<Integer> list=new ArrayList<>();
+    //  static void searchIndexAll(int [] arr,int target,int index){
+    //     if(index==arr.length){
+    //       return ;
+    //     }
+    //     if(arr[index]==target){
+    //     list.add(index);
+    //     System.out.println(list);
+    //     }
+    
+    //        searchIndexAll(arr, target, index + 1);
+        
+    //  }
+
+     static ArrayList<Integer> searchIndexAll(int [] arr,int target,int index, ArrayList<Integer> list){
+        if(index==arr.length){
+          return list;
+        }
+        if(arr[index]==target){
+        list.add(index);
+        
+        }
+    
+           return searchIndexAll(arr, target, index + 1, list);
+           
+        
+     }
+     
 }
