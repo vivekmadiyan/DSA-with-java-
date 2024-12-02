b69a6f6e35f57a023bf6d619afc37e645e348ee2
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-12-02 10:37:19 +0530

    Update T.java

--- RecursionPattern&Sort/T.java
+++ RecursionPattern&Sort/T.java
@@ -2,10 +2,11 @@ import java.util.Arrays;
 
 public class T {
     public static void main(String[] args) {
-        triangle(4, 0);
-        triangle2(4, 0);
+        // triangle(4, 0);
+        // triangle2(4, 0);
         int[] arr={4,3,2,1};
-        bubble(arr,arr.length-1, 0);
+        selection(arr,arr.length, 0, 0);
+        // bubble(arr,arr.length-1, 0);
         System.out.println(Arrays.toString(arr));
     }
     static void triangle(int r, int c) {
@@ -50,6 +51,30 @@ public class T {
         } else {
             
             bubble(arr,r - 1, 0);
+           
+        }
+    }
+
+    static void selection(int[] arr, int r, int c,int max) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            if(arr[c]>arr[max]){
+                selection(arr, r, c+1, c);
+            }else{
+                selection(arr, r,c + 1,max);
+            }
+            
+           
+           
+        } 
+        else {
+            int temp=arr[max];
+            arr[max]=arr[r-1];
+            arr[r-1]=temp;
+
+            selection(arr,r - 1, 0,0);
            
         }
     }
